438c3ec1c3be7d28f69bac95ea26441a506527de
Brandux Didier Juarez Avila <branduxjuarez@upeu.edu.pe>
2021-07-06 18:45:44 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+let express = require("express"),
+  path = require('path'),
+  nodeMailer = require('nodemailer'),
+  bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(express.static('src'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/send-email', function (req, res) {
+  let transporter = nodeMailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+          // Deberia poner el email que envia correos, darle permisos en el mismo gmail
+          // para que permita que esta aplicacion acceda y pueda enviar correos
+          // https://myaccount.google.com/lesssecureapps
+          user: 'example@gmail.com',
+          pass: 'test'
+      }
+  });
+  let mailOptions = {
+      // Eesto reemplazar con los datos que llegan del frontEnd
+      to: 'dbrandux@gmail.com',
+      subject: req.body.subject,
+      body: req.body.message
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  res.writeHead(301, { Location: 'index.html' });
+  res.end();
+});
+
+let server = app.listen(8081, function(){
+    let port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
